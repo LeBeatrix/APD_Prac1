b26ec3054e7252f0424d80750d74eb810c8fa70d
LeBeatrix <47351662+LeBeatrix@users.noreply.github.com>
2020-02-18 22:06:39 +0200

    Update App.java

--- src/main/java/com/sonia/App.java
+++ src/main/java/com/sonia/App.java
@@ -6,5 +6,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Assignment 1" );
+
+        //Cloned by Lene
+        System.out.println("Lene was here!");
     }
 }
